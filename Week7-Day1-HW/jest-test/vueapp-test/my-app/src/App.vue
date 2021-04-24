143c1e67e8ad0830b07e2301eef17c25ebfc2ff4
Rikin Prajapati <r.prajapati.150@westcliff.edu>
2021-04-23 23:21:55 -0700

    Week7-Day1-HW

---
+++ Week7-Day1-HW/jest-test/vueapp-test/my-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="Vue Logo">
+    <MessageComponent msg="This is my first VUE app!" />
+  </div>
+</template>
+
+<script>
+import MessageComponent from './components/MessageComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MessageComponent
+  }
+}
+</script>
+
+
